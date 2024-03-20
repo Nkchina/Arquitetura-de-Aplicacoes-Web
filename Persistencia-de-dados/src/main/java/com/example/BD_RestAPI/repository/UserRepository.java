aa9fc7fbaf497e14d95d1fe14702bcf0760dfb1a
Nicolas Chi <94817330+Nkchina@users.noreply.github.com>
2024-03-20 13:56:03 -0300

    Add files via upload

---
+++ Persistencia-de-dados/src/main/java/com/example/BD_RestAPI/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.BD_RestAPI.repository;
+import com.example.BD_RestAPI.model.UserEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<UserEntity, String> {
+}
