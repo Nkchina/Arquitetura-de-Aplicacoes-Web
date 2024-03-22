722cbe368c1b7c9ef9446465f5850a73dc6a7dc9
Nicolas Chi <94817330+Nkchina@users.noreply.github.com>
2024-03-21 23:03:04 -0300

    Update UserRepository.java

--- Persistencia-de-dados/src/main/java/com/example/BD_RestAPI/repository/UserRepository.java
+++ Persistencia-de-dados/src/main/java/com/example/BD_RestAPI/repository/UserRepository.java
@@ -5,4 +5,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends MongoRepository<UserEntity, String> {
+  
+    List<UserEntity> findByNomeIgnoreCase(String nome);
+    List<UserEntity> findByEmailIgnoreCase(String email);
+    List<UserEntity> findByNomeAndEmail(String nome, String email);
+    List<UserEntity> findByNomeStartingWithIgnoreCase(String prefixo);
+    List<UserEntity> findByNomeContainingIgnoreCase(String substring);
 }
